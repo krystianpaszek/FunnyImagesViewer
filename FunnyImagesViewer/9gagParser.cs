5d98d1de0cd7ffd84b1c19abfbe741dafe21bac2
krystianpaszek <krystianpaszek93@gmail.com>
2015-05-31 14:13:55 +0200

    Organized imports

--- FunnyImagesViewer/9gagParser.cs
+++ FunnyImagesViewer/9gagParser.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HtmlAgilityPack;
 using System.Net;
 
 namespace FunnyImagesViewer
