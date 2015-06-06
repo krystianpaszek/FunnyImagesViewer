bdd0d4c3d38815929525c7ac0272eb5e27e82563
krystianpaszek <krystianpaszek93@gmail.com>
2015-06-06 23:19:21 +0200

    Reorganized Kwejk Parser

--- FunnyImagesViewer/Parsers/KwejkParser.cs
+++ FunnyImagesViewer/Parsers/KwejkParser.cs
@@ -11,37 +11,67 @@ namespace FunnyImagesViewer.Parsers
     class KwejkParser : SiteParser
     {
         private const string baseAddress = "http://m.kwejk.pl";
+        private string nextPageAddress;
 
         public KwejkParser(Action<String> outputBoxSetter) : base(outputBoxSetter) { }
 
         protected override void fetchMoreImages()
         {
-            List<HtmlNode> links = getImagesNodes();
+            processNodes(getImagesNodes());
+        }
+
+        private string getNextPageAdress()
+        {
+            String address;
+
+            if (nextPageAddress == null)
+            {
+                address = baseAddress;
+            }
+            else
+            {
+                address = nextPageAddress;
+            }
+
+            return address;
         }
 
         private List<HtmlNode> getImagesNodes()
         {
             WebClient w = new WebClient();
-            //String outputString = w.DownloadString(baseAddress);
 
             HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-            doc.Load(w.OpenRead(baseAddress), Encoding.UTF8);
+            doc.Load(w.OpenRead(getNextPageAdress()), Encoding.UTF8);
 
             List<HtmlNode> results = doc.DocumentNode.Descendants().
                 Where(x => (x.Name == "a" && x.Attributes["data-track-action"] != null && x.Attributes["data-track-action"].Value.IndexOf("Tre") != -1)).ToList();
 
-            foreach (HtmlNode n in results)
+            HtmlNode nextPageNode = doc.DocumentNode.Descendants().
+                FirstOrDefault(x => (x.Name == "a" && x.Attributes["data-track-action"] != null && x.Attributes["data-track-action"].Value.IndexOf("Nast") != -1));
+
+            nextPageAddress = nextPageNode.GetAttributeValue("href", null);
+
+            return results;
+        }
+
+        private void processNodes(List<HtmlNode> nodes) {
+            foreach (HtmlNode n in nodes)
             {
-                System.Console.WriteLine(n.Descendants("img").ToList().First().GetAttributeValue("src", null).ToString());
-                System.Console.WriteLine(n.Descendants("img").ToList().First().GetAttributeValue("alt", null).ToString());
+                string link = n.Descendants("img").ToList().First().GetAttributeValue("src", null).ToString();
+                string title = n.Descendants("img").ToList().First().GetAttributeValue("alt", null).ToString();
                 HtmlNode parentNode = n.Ancestors().Where(x => (x.Name == "div" && x.Attributes["class"] != null && x.Attributes["class"].Value.Contains("media"))).ToList().First();
                 HtmlNode voteNode = parentNode.Descendants().Where(x => (x.Name == "div" && x.Attributes["class"] != null && x.Attributes["class"].Value.Contains("vote"))).ToList().First();
                 HtmlNode bubbleNode = voteNode.Descendants().Where(x => (x.Name == "span" && x.Attributes["class"] != null && x.Attributes["class"].Value.Contains("bubble"))).ToList().First();
-                System.Console.WriteLine(bubbleNode.InnerHtml);
 
+                int rating = Convert.ToInt32(bubbleNode.InnerHtml);
+
+                if (rating > 0)
+                {
+                    outputBoxSetter(link);
+                    SiteImage image = new SiteImage(link, title, "kwejk.pl");
+                    images.Add(image);
+                }
             }
-
-            return results;
         }
     }
 }
