c22d488d11d20e141a1c39843318f8875ae93219
krystianpaszek <krystianpaszek93@gmail.com>
2015-06-06 22:59:16 +0200

    Reorganization in Imgur Parser

--- FunnyImagesViewer/Parsers/ImgurParser.cs
+++ FunnyImagesViewer/Parsers/ImgurParser.cs
@@ -17,20 +17,16 @@ namespace FunnyImagesViewer.Parsers
 
         protected override void fetchMoreImages()
         {
-            WebRequest req = WebRequest.Create(baseUrl + "gallery/t/funny/viral/" + currentPage/60);
-            req.Method = "GET";
-            req.Headers["Authorization"] = "Client-ID " + client_id;
-            HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
-
-            Stream stream = resp.GetResponseStream();
-            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
-            String responseString = reader.ReadToEnd();
-
-            JObject joResponse = JObject.Parse(responseString);
-            JArray array = (JArray)joResponse["data"]["items"];
+            Stream stream = getNextResponse().GetResponseStream();
+            JArray array = parseResponseStream(stream);
 
             outputBoxSetter("Imgur:");
 
+            processNodes(array);
+        }
+
+        private void processNodes(JArray array)
+        {
             foreach (JToken jToken in array)
             {
                 SiteImage siteImage = JsonConvert.DeserializeObject<SiteImage>(jToken.ToString());
@@ -38,6 +34,26 @@ namespace FunnyImagesViewer.Parsers
                 images.Add(siteImage);
                 outputBoxSetter(siteImage.Address);
             }
+        }
+
+        private HttpWebResponse getNextResponse()
+        {
+            WebRequest req = WebRequest.Create(baseUrl + "gallery/t/funny/viral/" + currentPage);
+            req.Method = "GET";
+            req.Headers["Authorization"] = "Client-ID " + client_id;
+            HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
+
+            return resp;
+        }
+
+        private JArray parseResponseStream(Stream stream) {
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            String responseString = reader.ReadToEnd();
+
+            JObject joResponse = JObject.Parse(responseString);
+            JArray array = (JArray)joResponse["data"]["items"];
+
+            return array;
         }
     }
 }
